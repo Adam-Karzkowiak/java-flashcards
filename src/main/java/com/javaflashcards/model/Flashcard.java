873f9cc425246e112d5b1a27ddb66a4668e2b9ad
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-28 19:34:17 +0100

    Flashcard class fields created

--- src/main/java/com/javaflashcards/model/Flashcard.java
+++ src/main/java/com/javaflashcards/model/Flashcard.java
@@ -1,6 +1,19 @@
 package com.javaflashcards.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 class Flashcard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
     String question;
     String answer;
+
+
 }
