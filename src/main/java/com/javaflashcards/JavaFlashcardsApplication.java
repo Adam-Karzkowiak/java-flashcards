abb05bda63e3556dca13ae5002be402d5ecff7ba
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-28 19:27:53 +0100

    Initial commit

---
+++ src/main/java/com/javaflashcards/JavaFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package com.javaflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaflashcards/JavaFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
