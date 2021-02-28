e0cb2967ceeb22c3a1b0258bb32c628061fb4889
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-02-28 20:26:10 +0100

    Table name flashcards

--- src/main/java/com/javaflashcards/model/Flashcard.java
+++ src/main/java/com/javaflashcards/model/Flashcard.java
@@ -1,11 +1,8 @@
 package com.javaflashcards.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-
+@Table(name = "flashcards")
 @Entity
 public class Flashcard {
     @Id
