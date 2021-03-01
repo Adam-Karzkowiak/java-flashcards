bc5e28c34de640c3500c465146eb6825cd6eb8cc
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-01 22:17:49 +0100

    FlashcardRepository methods created

--- src/main/java/com/javaflashcards/data/FlashcardRepository.java
+++ src/main/java/com/javaflashcards/data/FlashcardRepository.java
@@ -1,5 +1,13 @@
 package com.javaflashcards.data;
 
 
+import com.javaflashcards.model.Flashcard;
+import com.javaflashcards.model.dto.FlashcardWriteModel;
+
 public interface FlashcardRepository {
+    Flashcard save(FlashcardWriteModel model);
+
+    Flashcard delete(int id);
+
+    Flashcard random();
 }
