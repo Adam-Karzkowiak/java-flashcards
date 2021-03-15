4bbc1cc24f0e6c032e6258003f7f3f8474a30cab
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-15 22:26:03 +0100

    Simplified-now we are using only FlashcardRepository

--- src/main/java/com/javaflashcards/data/FlashcardRepository.java
+++ src/main/java/com/javaflashcards/data/FlashcardRepository.java
@@ -2,9 +2,14 @@ package com.javaflashcards.data;
 
 
 import com.javaflashcards.model.Flashcard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FlashcardRepository {
+@Repository
+public interface FlashcardRepository extends JpaRepository<Integer, Flashcard> {
     Flashcard save(Flashcard model);
 
+    Flashcard getOne(int id);
 
+    long count();
 }
--- src/main/java/com/javaflashcards/data/SqlFlashcardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaflashcards.data;
-
-import com.javaflashcards.model.Flashcard;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface SqlFlashcardRepository extends FlashcardRepository, JpaRepository<Flashcard, Integer> {
-}
