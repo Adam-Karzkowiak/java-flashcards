624735aec48f4cfee4d187d387418c4ef4e0c838
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-16 23:01:03 +0100

    default method to create FlashcardQuestionReadModel

--- src/main/java/com/javaflashcards/model/dto/FlashcardQuestionReadModel.java
+++ src/main/java/com/javaflashcards/model/dto/FlashcardQuestionReadModel.java
@@ -2,4 +2,12 @@ package com.javaflashcards.model.dto;
 
 public class FlashcardQuestionReadModel {
     String question;
+
+    private FlashcardQuestionReadModel(String question) {
+        this.question = question;
+    }
+
+    public static FlashcardQuestionReadModel create(String question) {
+        return new FlashcardQuestionReadModel(question);
+    }
 }
