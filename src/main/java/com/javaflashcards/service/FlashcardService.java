8827cd12a049cfee4a424035aa3b466335c922f8
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-16 23:13:34 +0100

    randomFlashcard method created

--- src/main/java/com/javaflashcards/service/FlashcardService.java
+++ src/main/java/com/javaflashcards/service/FlashcardService.java
@@ -2,12 +2,16 @@ package com.javaflashcards.service;
 
 import com.javaflashcards.data.FlashcardRepository;
 import com.javaflashcards.model.Flashcard;
+import com.javaflashcards.model.dto.FlashcardQuestionReadModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
+
 @Service
 public class FlashcardService {
-    private FlashcardRepository flashcardRepository;
+    private final FlashcardRepository flashcardRepository;
 
     @Autowired
     FlashcardService(final FlashcardRepository flashcardRepository) {
@@ -16,5 +20,13 @@ public class FlashcardService {
 
     public Flashcard saveEntity(Flashcard entity) {
         return flashcardRepository.save(entity);
+    }
+
+    public FlashcardQuestionReadModel randomFlashcard() {
+        Random random = new Random();
+        Long maxValue = flashcardRepository.count();
+        int id = random.nextInt(Math.toIntExact(maxValue)) + 1;
+        Flashcard flashcard = flashcardRepository.getOne(id);
+        return FlashcardQuestionReadModel.create(flashcard.getQuestion());
     }
 }
