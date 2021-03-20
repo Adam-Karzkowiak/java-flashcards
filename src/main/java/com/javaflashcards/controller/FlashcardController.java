8b4d6860fb1c8acdf10209741fc3cde40797514a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-20 19:27:06 +0100

    Added: Logger - warning when exposing all flashcards Created methods: getFlashcardById() getAll() Fixed: randomFlashcardQuestion()- fixed 406 problem.

--- src/main/java/com/javaflashcards/controller/FlashcardController.java
+++ src/main/java/com/javaflashcards/controller/FlashcardController.java
@@ -1,19 +1,23 @@
 package com.javaflashcards.controller;
 
 import com.javaflashcards.model.Flashcard;
+import com.javaflashcards.model.dto.FlashcardQuestionReadModel;
 import com.javaflashcards.service.FlashcardService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class FlashcardController {
 
     private FlashcardService flashcardService;
+    private static final Logger logger = LoggerFactory.getLogger(FlashcardController.class);
+
 
     @Autowired
     FlashcardController(final FlashcardService flashcardService) {
@@ -21,8 +25,24 @@ public class FlashcardController {
     }
 
     @PostMapping("/flashcards")
-    ResponseEntity<Flashcard> createNewFlashcard(@RequestBody Flashcard model){
+    public ResponseEntity<Flashcard> createNewFlashcard(@RequestBody Flashcard model) {
         Flashcard savedFlashcard = flashcardService.saveEntity(model);
-        return ResponseEntity.created(URI.create("/"+savedFlashcard.getId())).body(savedFlashcard);
+        return ResponseEntity.created(URI.create("/" + savedFlashcard.getId())).body(savedFlashcard);
+    }
+
+    @GetMapping("/flashcards")
+    public ResponseEntity<FlashcardQuestionReadModel> randomFlashcardQuestion() {
+        return ResponseEntity.ok(flashcardService.randomFlashcard());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Flashcard>> getAll() {
+        logger.warn("Exposing all flashcards!");
+        return ResponseEntity.ok(flashcardService.showAllFlashcards());
+    }
+
+    @GetMapping("/flashcards/{id}")
+    public ResponseEntity<Flashcard> getFlashcardById(@PathVariable int id) {
+        return ResponseEntity.ok(flashcardService.findOneById(id));
     }
 }
