89aa51e2c490b570cd406b306825aa5f6bd3d244
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-20 19:04:09 +0100

    getOne() method changed to findById() - problems with lazy load fixed

--- src/main/java/com/javaflashcards/data/FlashcardRepository.java
+++ src/main/java/com/javaflashcards/data/FlashcardRepository.java
@@ -5,11 +5,16 @@ import com.javaflashcards.model.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface FlashcardRepository extends JpaRepository<Integer, Flashcard> {
+public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
     Flashcard save(Flashcard model);
 
-    Flashcard getOne(int id);
+    Optional<Flashcard> findById(Integer id);
 
     long count();
+
+    List<Flashcard> findAll();
 }
