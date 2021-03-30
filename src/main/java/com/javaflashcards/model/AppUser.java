19c2e165b3b717f39325152145be8e39a39bf94f
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-03-30 20:40:58 +0200

    AppUser model created

---
+++ src/main/java/com/javaflashcards/model/AppUser.java
@@ -0,0 +1,43 @@
+package com.javaflashcards.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+
+@Entity
+@Data
+@Table(name = "app_users")
+class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    private String login;
+    @NotNull
+    private String password;
+    @Email
+    private String email;
+
+    @OneToMany
+    private Set<Flashcard> guessed;
+
+    @OneToMany
+    private Set<Flashcard> notGuessed;
+
+    private AppUser(String login, String password, String email) {
+        this.login = login;
+        this.password = password;
+        this.email = email;
+    }
+
+
+    public static AppUser create(String login, String password, String email) {
+        return new AppUser(login, password, email);
+    }
+
+}
