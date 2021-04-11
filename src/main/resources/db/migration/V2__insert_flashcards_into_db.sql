e458ba71741061968ffe087507422656692ada62
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-11 22:42:10 +0200

    Flyway migration - testing sql script to insert data into db

--- src/main/resources/db/migration/V2__insert_flashcards_into_db.sql
+++ src/main/resources/db/migration/V2__insert_flashcards_into_db.sql
@@ -1,4 +1,4 @@
-INSERT INTO flashcards (id, question, answer) VALUES (1,'Programowanie obiektowe','Oznacza to, że wykorzystuje obiektowy paradygmat programowania. Program definiuje się za pomocą obiektów-elementów łączących stan (dane-pola klasy) i zachowanie (procedury-metody)
+INSERT INTO flashcards (question, answer) VALUES ('Programowanie obiektowe','Oznacza to, że wykorzystuje obiektowy paradygmat programowania. Program definiuje się za pomocą obiektów-elementów łączących stan (dane-pola klasy) i zachowanie (procedury-metody)
 Główny koncepty Javy:
 1)Klasa-szablon/forma służąca do zdefiniowania danych oraz zachowań obiektu
 2)Obiekt-instancja klasy
