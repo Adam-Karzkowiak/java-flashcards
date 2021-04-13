7929195f36e11a6ce3f44aed311f6536216f7e27
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-13 13:24:48 +0200

    Flyway migration - version changed

--- src/main/resources/db/migration/V2__init_table_flashcards.sql
+++ src/main/resources/db/migration/V2__init_table_flashcards.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS flashcards;
-CREATE TABLE flashcards(
+CREATE TABLE IF NOT EXISTS flashcards(
                       id INT GENERATED ALWAYS AS IDENTITY,
                       question VARCHAR(255) NOT NULL ,
                       answer VARCHAR(1000) NOT NULL
