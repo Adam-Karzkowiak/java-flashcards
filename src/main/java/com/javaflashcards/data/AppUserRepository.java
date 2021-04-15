5b0213431b9f8b2312d490f78b2970156ed21b8b
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-15 23:06:20 +0200

    AppUserRepository created

---
+++ src/main/java/com/javaflashcards/data/AppUserRepository.java
@@ -0,0 +1,19 @@
+package com.javaflashcards.data;
+
+import com.javaflashcards.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    AppUser findByUsername(String username);
+
+    boolean existsById(Long id);
+
+    AppUser save(AppUser entity);
+
+    void deleteById(Integer id);
+
+    AppUser getOne(int id);
+}
