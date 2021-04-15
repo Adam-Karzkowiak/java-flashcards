1d2bb4df1ac5e82963183e4e70cf75c89b8d7c0a
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-15 22:57:43 +0200

    static create method added

--- src/main/java/com/javaflashcards/model/dto/FlashcardAnswerReadModel.java
+++ src/main/java/com/javaflashcards/model/dto/FlashcardAnswerReadModel.java
@@ -2,4 +2,12 @@ package com.javaflashcards.model.dto;
 
 public class FlashcardAnswerReadModel {
     String answer;
+
+    private FlashcardAnswerReadModel(String answer){
+        this.answer=answer;
+    }
+
+    public static FlashcardAnswerReadModel create(String answer){
+        return new FlashcardAnswerReadModel(answer);
+    }
 }
