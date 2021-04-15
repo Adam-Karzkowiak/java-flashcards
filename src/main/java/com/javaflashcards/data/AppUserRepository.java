ec994b5a1c642eb4f9982a99c880a32527963210
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-04-15 23:08:32 +0200

    access set to public

--- src/main/java/com/javaflashcards/data/AppUserRepository.java
+++ src/main/java/com/javaflashcards/data/AppUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+public
 interface AppUserRepository extends JpaRepository<AppUser, Integer> {
 
     AppUser findByUsername(String username);
