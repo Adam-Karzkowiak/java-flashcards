b37a6aea8f8bc122cb445881ad604945a63bbe0c
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-06-14 21:52:13 +0200

    created user service test class

---
+++ src/test/java/com/javaflashcards/service/AppUserServiceTest.java
@@ -0,0 +1,23 @@
+package com.javaflashcards.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserServiceTest {
+
+
+    @Test
+    @DisplayName("should add flashcard to user guessed set")
+    void addFlashcardToUserGuessedSet() {
+
+    }
+
+    @Test
+    @DisplayName("should add flashcard to user not guessed set")
+    void addFlashcardToUserNotGuessedSet() {
+
+    }
+
+}
